84badb655982e9c22568e82cf0dfbba0bdb91873
dksgmlwls <dksgmlwlsim@gmail.com>
2022-07-21 16:50:53 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:ocr/pages/home_page.dart';
 import 'package:ocr/pages/initial_page.dart';
 import 'package:ocr/pages/auths/login_page.dart';
 import 'package:ocr/pages/auths/register_page.dart';
+import 'package:ocr/providers/auth_provider.dart';
 import '/pages/initial_page.dart';
 
 void main() async{
@@ -57,11 +58,7 @@ class Login extends State<FormScreen> {
 
                   ElevatedButton(
                     onPressed: () {
-                      //!!!! 중요
-                      //pop을 이용해 데이터를 반환할때는 arguments클래스를 생성하면서 returnValue 에 데이터를 실어서 보낸다.
-                      //Navigator.pop(context, Arguments(returnValue: ReturnValue(result:'Yep!')));
-                      //Navigator.of(context).pushNamed(LoginPage.routeName);
-                      Navigator.push( // SecondPage로 화면 이동 코드.
+                      Navigator.push(
                         context,
                         MaterialPageRoute(builder: (context) => LoginPage()),
                         // MaterialPageRoute는 머테리얼 디자인으로 작성된 페이지 사이에 화면 전환을 할 때 사용된다.
@@ -73,11 +70,6 @@ class Login extends State<FormScreen> {
 
                   ElevatedButton(
                     onPressed: () {
-                      // Navigator.push(
-                      //   context,
-                      //   //MaterialPageRoute(builder: (_) => Register2()),
-                      // );
-                     //Navigator.of(context).pushNamed(SignUpPage.routeName);
                       Navigator.push( // SecondPage로 화면 이동 코드.
                         context,
                         MaterialPageRoute(builder: (context) => SignUpPage()),
@@ -90,11 +82,7 @@ class Login extends State<FormScreen> {
 
                   ElevatedButton(
                     onPressed: () {
-                      // Navigator.push(
-                      //   context,
-                      //   //MaterialPageRoute(builder: (_) => Register2()),
-                      // );
-                      //Navigator.of(context).pushNamed(SignUpPage.routeName);
+
                       Navigator.push( // SecondPage로 화면 이동 코드.
                         context,
                         MaterialPageRoute(builder: (context) => HomePage()),
@@ -114,27 +102,25 @@ class Login extends State<FormScreen> {
 
 }
 
-
-class SignUpPage extends StatefulWidget {
-  static const routeName = "/signUp";
+class LoginPage extends StatefulWidget {
+  static const routeName = "/login";
 
   @override
-  _SignUpPageState createState() => _SignUpPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _SignUpPageState extends State<SignUpPage> {
+class _LoginPageState extends State<LoginPage> {
   final formKey = GlobalKey<FormState>();
-  late String _name;
-  late String _password;
-  late String _email;
-  late String _phoneNumber;
+  var _isLoading = false;
+  late String? _id;
+  late String? _password;
 
   void _showErrorDialog(String message) {
     showDialog(
       context: context,
       builder: (ctx) => AlertDialog(
         title: Text(
-          "회원가입 실패",
+          "로그인 실패",
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         content: Text(message),
@@ -150,26 +136,221 @@ class _SignUpPageState extends State<SignUpPage> {
     );
   }
 
-  Future<void> _submit() async {
-    if (formKey.currentState!.validate() == false) {
-      return;
-    } else {
-      formKey.currentState!.save();
-      try {
-        await Provider.of<AuthProvider>(context, listen: false)
-            .signUp(_name, _phoneNumber, _email, _password);
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text("가입이 완료되었습니다. 로그인을 진행해주세요."),
-            duration: Duration(seconds: 1),
+  // Future<void> _submit() async {
+  //   if (formKey.currentState!.validate() == false) {
+  //     return;
+  //   } else {
+  //     formKey.currentState!.save();
+  //     try {
+  //       setState(() {
+  //         _isLoading = true;
+  //       });
+  //       await Provider.of<AuthProvider>(context, listen: false)
+  //           .login(_id, _password);
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //       if (Provider.of<AuthProvider>(context, listen: false).isAuth) {
+  //         await FlutterSecureStorage().write(key: "userEmail", value: _id);
+  //         ScaffoldMessenger.of(context).showSnackBar(
+  //           SnackBar(
+  //             content: Text("로그인에 성공하였습니다."),
+  //             duration: Duration(seconds: 1),
+  //           ),
+  //         );
+  //         Navigator.of(context).pop();
+  //       }
+  //     } catch (error) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //       _showErrorDialog("아이디 또는 비밀번호를 확인해주세요.");
+  //     }
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "로그인",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            height: MediaQuery.of(context).size.height * 0.8,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  //child: Image.asset("images/initial_4.png"),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 10, bottom: 10, left: 20, right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          hintText: "계정 이메일을 입력해주세요.",
+                        ),
+                        onSaved: (value) {
+                          _id = value;
+                        },
+                        // validator: (value) {
+                        //   if (value!.length < 10) {
+                        //     return "이메일 형식에 맞게 입력해주세요.";
+                        //   }
+                        // },
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        child: TextFormField(
+                          obscureText: true,
+                          keyboardType: TextInputType.visiblePassword,
+                          textInputAction: TextInputAction.done,
+                          decoration: InputDecoration(
+                            hintText: "비밀번호를 입력해주세요.",
+                          ),
+                          onSaved: (value) {
+                            _password = value;
+                          },
+                          // validator: (value) {
+                          //   if (value!.length < 10) {
+                          //     return "비밀번호는 10자 이상입니다.";
+                          //   }
+                          // },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                _isLoading
+                    ? Container(
+                  alignment: Alignment.center,
+                  height: 40,
+                  child: CircularProgressIndicator(),
+                )
+                    : FlatButton(
+                  child: Container(
+                    height: 40,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey,
+                    ),
+                    child: Text("로그인",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center),
+                  ),
+                  onPressed: () {
+                    //_submit();
+                    Navigator.of(context).pop();
+                  },
+                ),
+                FlatButton(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "이메일 또는 비밀번호를 잊으셨나요?",
+                      style: TextStyle(color: Colors.blue, fontSize: 10),
+                    ),
+                  ),
+                  onPressed: () {
+                    //Navigator.of(context).pushNamed(FindUserInfoPage.routeName);
+                  },
+                ),
+              ],
+            ),
           ),
-        );
-        Navigator.of(context).pop();
-      } catch (error) {
-        _showErrorDialog("이미 존재하는 계정입니다.");
-      }
-    }
+        ),
+      ),
+    );
   }
+}
+
+
+class SignUpPage extends StatefulWidget {
+  static const routeName = "/signUp";
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final formKey = GlobalKey<FormState>();
+  late String _name;
+  late String _password;
+  late String _email;
+  late String _phoneNumber;
+  //
+  // void _showErrorDialog(String message) {
+  //   showDialog(
+  //     context: context,
+  //     builder: (ctx) => AlertDialog(
+  //       title: Text(
+  //         "회원가입 실패",
+  //         style: TextStyle(fontWeight: FontWeight.bold),
+  //       ),
+  //       content: Text(message),
+  //       actions: <Widget>[
+  //         FlatButton(
+  //           child: Text("예"),
+  //           onPressed: () {
+  //             Navigator.of(ctx).pop();
+  //           },
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+  //
+  // Future<void> _submit() async {
+  //   if (formKey.currentState!.validate() == false) {
+  //     return;
+  //   } else {
+  //     formKey.currentState!.save();
+  //     try {
+  //       await Provider.of<AuthProvider>(context, listen: false)
+  //           .signUp(_name, _phoneNumber, _email, _password);
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(
+  //           content: Text("가입이 완료되었습니다. 로그인을 진행해주세요."),
+  //           duration: Duration(seconds: 1),
+  //         ),
+  //       );
+  //       Navigator.of(context).pop();
+  //     } catch (error) {
+  //       _showErrorDialog("이미 존재하는 계정입니다.");
+  //     }
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -392,3 +573,4 @@ class _SignUpPageState extends State<SignUpPage> {
 //   );
 // }
 }
+

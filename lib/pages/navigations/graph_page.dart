55f12e1d87058a231e89cbf30f2388724d789079
dksgmlwlsim <dksgmlwlsim@gmail.com>
2022-08-18 11:11:26 +0900

    ten commit

--- lib/pages/navigations/graph_page.dart
+++ lib/pages/navigations/graph_page.dart
@@ -24,79 +24,39 @@ class GraphPage extends StatefulWidget {
 }
 
 class _GraphPageState extends State<GraphPage> {
-  File? _image;
-  final picker = ImagePicker();
-
-  // 비동기 처리를 통해 카메라와 갤러리에서 이미지를 가져온다.
-  Future getImage(ImageSource imageSource) async {
-    final image = await picker.pickImage(source: imageSource);
-
-    setState(() {
-      _image = File(image!.path); // 가져온 이미지를 _image에 저장
-    });
-  }
-
-  // 이미지를 보여주는 위젯
-  Widget showImage() {
-    return Container(
-        color: const Color(0xffd0cece),
-        width: MediaQuery
-            .of(context)
-            .size
-            .width,
-        height: MediaQuery
-            .of(context)
-            .size
-            .width,
-        child: Center(
-            child: _image == null
-                ? Text('No image selected.')
-                : Image.file(File(_image!.path))));
-  }
+  //const GraphPage({Key? key, required this.title}) : super(key: key);
+  //final String title;
+  late final DateTime _selectedDate;
 
   @override
   Widget build(BuildContext context) {
-    // 화면 세로 고정
-    SystemChrome.setPreferredOrientations(
-        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
     return Scaffold(
-        backgroundColor: const Color(0xfff4f3f9),
-        body: Column(
+      appBar: AppBar(
+        title: Text('현황 보기'),
+      ),
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 25.0),
-            showImage(),
-            SizedBox(
-              height: 50.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                // 카메라 촬영 버튼
-                FloatingActionButton(
-                  heroTag: "camera",
-                  child: Icon(Icons.add_a_photo),
-                  tooltip: 'pick Iamge',
-                  onPressed: () {
-                    getImage(ImageSource.camera);
-                    // Navigator.push(context, MaterialPageRoute(
-                    //     builder: (context) => CameraOverlay()));
-                  },
-                ),
+          children: <Widget>[
+            RaisedButton(
+                child: Text('Run'),
+                color: Colors.blue,
+                textColor: Colors.white,
+                onPressed: (){
+                  Future<DateTime> future = showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2022),
+                      lastDate: DateTime(2050),
+                      builder: (BuildContext context, Widget child){
 
-                // 갤러리에서 이미지를 가져오는 버튼
-                FloatingActionButton(
-                  heroTag: "send_result",
-                  child: Icon(Icons.check_outlined),
-                  tooltip: 'pick Iamge',
-                  onPressed: () {
-                    //getImage(ImageSource.gallery);
-                  },
-                ),
-              ],
-            )
+                  }
+                }
           ],
-        ));
+        ),
+      ),
+
+
+    );
   }
 }
